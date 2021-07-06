156732596d79cc9891a08668e18306b4d7255f48
fsabirov <sabirovfarit@gmail.com>
2021-07-06 11:06:32 +0300

    fix empty item,font-size footer, App to a separate file, class components

---
+++ src/components/app/app.js
@@ -0,0 +1,132 @@
+import React, { Component } from "react";
+
+import AppHeader from "../app-header/app-header";
+import NewTaskForm from "../new-tast-form/new-tast-form";
+import TaskList from "../task-list/task-list";
+import Footer from "../footer/footer";
+
+import "./app.css";
+
+export default class App extends Component {
+  maxId = 100;
+
+  static propTypes = {};
+
+  state = {
+    todoData: [
+      this.createToDoItem("Drink Coffee"),
+      this.createToDoItem("Make awesome App"),
+      this.createToDoItem("Have a lunch"),
+    ],
+    filter: "all",
+  };
+
+  addItem = (text) => {
+    this.setState(({ todoData }) => {
+      const newArr = [...todoData, this.createToDoItem(text)];
+      return {
+        todoData: newArr,
+        filter: "all",
+      };
+    });
+  };
+
+  clearCompleted = () => {
+    this.setState(({ todoData, filter }) => {
+      const newArr = todoData.filter((item) => !item.done);
+      return {
+        todoData: newArr,
+        filter,
+      };
+    });
+  };
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const before = todoData.slice(0, idx);
+      const after = todoData.slice(idx + 1);
+
+      const newArray = [...before, ...after];
+      return {
+        todoData: newArray,
+        filter: "all",
+      };
+    });
+  };
+
+  onToggleDone = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id);
+
+      const oldItem = todoData[idx];
+      const newItem = { ...oldItem, done: !oldItem.done };
+      const before = todoData.slice(0, idx);
+      const after = todoData.slice(idx + 1);
+
+      const newArray = [...before, newItem, ...after];
+      return {
+        todoData: newArray,
+        filter: "all",
+      };
+    });
+  };
+
+  createToDoItem(label) {
+    return {
+      label,
+      done: false,
+      id: this.maxId++,
+      time: new Date(),
+    };
+  }
+
+  filterActive = (filter) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData,
+        filter,
+      };
+    });
+  };
+
+  showItems(items) {
+    switch (this.state.filter) {
+      case "all":
+        return items;
+      case "active":
+        return items.filter((item) => !item.done);
+      case "completed":
+        return items.filter((item) => item.done);
+      default:
+        return "all";
+    }
+  }
+
+  render() {
+    const toDo = this.state.todoData.filter((el) => !el.done).length;
+
+    return (
+      <section className="todoapp">
+        <header className="header">
+          <AppHeader />
+          <NewTaskForm onItemAdded={this.addItem} />
+        </header>
+        <section className="main">
+          <TaskList
+            todos={this.showItems(this.state.todoData)}
+            onDeleted={this.deleteItem}
+            onToggleDone={this.onToggleDone}
+          />
+          <Footer
+            toDo={toDo}
+            filterActiveFooter={this.filterActive}
+            filter={this.state.filter}
+            clearCompleted={this.clearCompleted}
+          />
+        </section>
+      </section>
+    );
+  }
+}
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,56 +1,55 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./footer.css";
 import PropTypes from "prop-types";
 
-export default class Footer extends Component {
-  static defaultProps = {
-    filter: "all",
-  };
+const Footer = ({ toDo, filterActiveFooter, filter, clearCompleted }) => {
+  return (
+    <footer className="footer">
+      <span className="todo-count">{toDo} items left</span>
+      <ul className="filters">
+        <li>
+          <button
+            className={filter === "all" ? "selected" : ""}
+            onClick={() => filterActiveFooter("all")}
+          >
+            All
+          </button>
+        </li>
+        <li>
+          <button
+            className={filter === "active" ? "selected" : ""}
+            onClick={() => filterActiveFooter("active")}
+          >
+            Active
+          </button>
+        </li>
+        <li>
+          <button
+            className={filter === "completed" ? "selected" : ""}
+            onClick={() => filterActiveFooter("completed")}
+          >
+            Completed
+          </button>
+        </li>
+      </ul>
+      <button className="clear-completed" onClick={() => clearCompleted()}>
+        Clear completed
+      </button>
+    </footer>
+  );
+};
 
-  static propTypes = {
-    filterActiveFooter: PropTypes.func,
-    onToggleDone: PropTypes.func,
-    clearCompleted: PropTypes.func,
-    toDo: PropTypes.number,
-    filter: PropTypes.string,
-  };
+Footer.defaultProps = {
+  toDo: 0,
+  filter: "all",
+};
 
-  render() {
-    const { toDo, filterActiveFooter, filter, clearCompleted } = this.props;
-    return (
-      <footer className="footer">
-        <span className="todo-count">{toDo} items left</span>
-        <ul className="filters">
-          <li>
-            <button
-              className={filter === "all" ? "selected" : ""}
-              onClick={() => filterActiveFooter("all")}
-            >
-              All
-            </button>
-          </li>
-          <li>
-            <button
-              className={filter === "active" ? "selected" : ""}
-              onClick={() => filterActiveFooter("active")}
-            >
-              Active
-            </button>
-          </li>
-          <li>
-            <button
-              className={filter === "completed" ? "selected" : ""}
-              onClick={() => filterActiveFooter("completed")}
-            >
-              Completed
-            </button>
-          </li>
-        </ul>
-        <button className="clear-completed" onClick={() => clearCompleted()}>
-          Clear completed
-        </button>
-      </footer>
-    );
-  }
-}
+Footer.propTypes = {
+  toDo: PropTypes.number,
+  filter: PropTypes.string,
+  clearCompleted: PropTypes.func,
+  filterActiveFooter: PropTypes.func,
+};
+
+export default Footer;
